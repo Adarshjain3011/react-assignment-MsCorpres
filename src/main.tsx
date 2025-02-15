b744ec2decb2d7178a1c0c6c04b64aa5574058b1
Adarshjain3011 <adarshjain3011@gamil.com>
2025-02-15 21:10:43 +0530

    setup store

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,29 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { Store } from "@reduxjs/toolkit";
+import App from "./App";
+import theme from "./theme/theme.ts";
+import "./index.css";
 
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
+const queryClient = new QueryClient();
 
-import theme from './theme/theme.ts';
-
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
   </StrictMode>
 );
+
+
